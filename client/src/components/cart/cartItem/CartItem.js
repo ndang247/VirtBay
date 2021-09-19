18303267b58f78ee0b9f7c9e62e0e2d6a556c934
ndang <nathandang73@gmail.com>
2021-09-20 02:13:56 +1000

    Add spacing

--- client/src/components/cart/cartItem/CartItem.js
+++ client/src/components/cart/cartItem/CartItem.js
@@ -13,6 +13,7 @@ const CartItem = ({ item, handleUpdateQty, handleRemoveFromCart }) => {
             <CardMedia image={item.media.source} alt={item.name} className={classes.media} />
             <CardContent className={classes.cardContent}>
                 <Typography variant='h6'>{item.name}</Typography>
+                &nbsp;
                 <Typography variant='h6'>{item.line_total.formatted_with_symbol}</Typography>
             </CardContent>
             <CardActions className={classes.cartActions}>
